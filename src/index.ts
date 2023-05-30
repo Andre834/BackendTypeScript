d07827d4f5aefa25bee9e7ee3497f0b6296cfbe6
Andre Santos <63032209+Andre834@users.noreply.github.com>
2023-05-29 21:02:41 -0300

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const a:string = "Santiago";
-console.log(a);
+import express from 'express';
+
+const server = express();
+
+server.get('/', (req, res ) =>  {
+    res.send('hello world')
+})
+
+server.listen(3333, () => {
+    console.log('Servidor rodando na porta 3333')
+})
 
